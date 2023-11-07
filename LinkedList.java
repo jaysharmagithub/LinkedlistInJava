0b21a6d03f4ce8e8eb2a6cac632b57f9462009e5
jaysharmagithub <125895781+jaysharmagithub@users.noreply.github.com>
2023-11-07 12:22:56 +0530

    LinkedList in Java
    A Linkedlist implementation in java

---
+++ LinkedList.java
@@ -0,0 +1,63 @@
+public class LinkedList {
+
+    public Node head;
+
+    public LinkedList(){
+        this.head = null;
+    }
+
+    public void addToHead(String data ){
+        Node newHead = new Node(data);
+        Node currentHead = this.head;
+        this.head = newHead;
+        if(currentHead != null) {
+            this.head.setNextNode(currentHead);
+        }
+    }
+
+    public void addToTail(String data){
+        Node tail = this.head;
+        if (tail == null){
+            this.head = new Node(data);
+        }
+        else {
+            while(tail.getNextNode() != null){
+                tail = tail.getNextNode();
+            }
+             tail.setNextNode(new Node(data));
+        }
+
+    }
+    public String removeHead(){
+
+        Node removeHead = this.head;
+        if(removeHead == null){
+            return null;
+        }
+        this.head = removeHead.getNextNode();
+        return removeHead.data;
+    }
+
+    public String printList(){
+        String output = "<head>";
+        Node currentNode = this.head;
+        while(currentNode != null){
+            output += currentNode.data + " ";
+            currentNode = currentNode.getNextNode();
+        }
+        output += "<tail>";
+        System.out.println(output);
+        return output;
+    }
+    public static void main(String[] args) {
+        LinkedList seasons = new LinkedList();
+        seasons.addToHead("summer");
+        seasons.addToHead("spring");
+        seasons.printList();
+        seasons.addToTail("fall");
+        seasons.addToTail("winter");
+        seasons.printList();
+        seasons.removeHead();
+        seasons.printList();
+    }
+}
